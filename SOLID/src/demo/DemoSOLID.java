4050fa544364ddcee4c2613074d9adafd4d77446
namnh <33839115+namnh43@users.noreply.github.com>
2023-04-17 22:24:06 +0700

    Create DemoSOLID.java

---
+++ SOLID/src/demo/DemoSOLID.java
@@ -0,0 +1,45 @@
+package demo;
+
+interface Shape {
+    double area();
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double area() {
+        return width * height;
+    }
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double area() {
+        return 0.5 * base * height;
+    }
+}
